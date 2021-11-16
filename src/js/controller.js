58208241d1275cda2c831569d29826cbf7050bf8
Patrick <patricklee0221@gmail.com>
2021-11-16 23:21:31 +0000

    create pagination (10 results per page)

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,7 +36,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
   } catch (error) {
     console.log(error);
   }
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config';
+import { API_URL, RES_PER_PAGE } from './config';
 import { getJSON } from './helper';
 
 export const state = {
@@ -7,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -54,4 +56,10 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-loadSearchResults('pizza');
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * RES_PER_PAGE;
+  const end = page * RES_PER_PAGE;
+
+  return state.search.results.slice(start, end);
+};
